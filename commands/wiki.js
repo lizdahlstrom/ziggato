d1636e600042422f40c43efab717b398208d47db
Liz Dahlström <liz.dahlstrom@gmail.com>
2020-07-15 14:18:42 +0200

    Fix so that wiki searches are case insensitive

--- commands/wiki.js
+++ commands/wiki.js
@@ -3,14 +3,26 @@ const Discord = require('discord.js');
 const { palette } = require('../config.json');
 
 const callWiki = async (msg, args) => {
-  const searchStr = args.join('%20');
-  const api = `https://en.wikipedia.org/w/api.php?format=json&action=query&titles=${searchStr}`;
-  const url = api + '&prop=extracts&exintro&explaintext&redirects=1';
-  const imageURL = api + '&prop=pageimages&pithumbsize=100';
+  let searchStr = args.join('%20');
+  const api = 'https://en.wikipedia.org/w/api.php';
+  // first search, get the right capitalization, then query
+  const apiSearch =
+    api +
+    `?action=opensearch&search=${searchStr}&limit=1&namespace=0&format=json`;
+  // search
+  let search = await fetch(apiSearch);
+  search = await search.json();
+
+  searchStr = search[1][0].replace(' ', '%20');
+
+  const query = api + `?format=json&action=query&titles=${searchStr}`;
+  const url = query + '&prop=extracts&exintro&explaintext&redirects=1';
+  const imageURL = query + '&prop=pageimages&pithumbsize=100';
 
   const maxLength = 880;
   const embed = new Discord.MessageEmbed();
 
+  // get excerpt
   let result = await fetch(url);
   result = await result.json();
   result = Object.values(result.query.pages)[0];
@@ -19,18 +31,21 @@ const callWiki = async (msg, args) => {
 
   if (!pageID) throw new Error(`Nopes! Couldn't find that in da wiki 😿`);
 
+  // get image
   let img = await fetch(imageURL);
   img = await img.json();
-  img = img.query.pages[pageID]
-    ? img.query.pages[pageID].thumbnail.source
-    : null;
+  img =
+    img.query.pages[pageID] && img.query.pages[pageID].thumbnail
+      ? img.query.pages[pageID].thumbnail.source
+      : null;
+
   if (img) embed.setThumbnail(img);
 
   let title = result.title;
   let extract = result.extract;
 
-  const resultStr =
-    extract.substring(0, (extract + '.').lastIndexOf('.', maxLength)) + '.';
+  const excerpt =
+    extract.substring(0, (extract + '.').lastIndexOf('.', maxLength)) + '...';
 
   embed.setColor(palette.dark);
   embed.setAuthor(
@@ -41,7 +56,7 @@ const callWiki = async (msg, args) => {
 
   embed.setTitle(title);
   embed.setURL(`https://en.wikipedia.org/?curid=${pageID}`);
-  embed.setDescription(resultStr);
+  embed.setDescription(excerpt);
   embed.setFooter(
     `Requested by ${msg.author.username}`,
     msg.author.displayAvatarURL
