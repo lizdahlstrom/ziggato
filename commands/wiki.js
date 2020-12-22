63579cd49f1c0a429762f312223338bcdf2019c8
Liz Dahlström <liz.dahlstrom@gmail.com>
2020-12-22 11:35:12 +0100

    Refactor wiki code

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const {palette} = require('../config.json');
+const api = 'https://en.wikipedia.org/w/api.php';
 
 const buildEmbed = (title, pageID, excerpt, msg) => {
   return new Discord.MessageEmbed()
@@ -20,9 +21,7 @@ const buildEmbed = (title, pageID, excerpt, msg) => {
       .setTimestamp(new Date());
 };
 
-const callWiki = async (msg, args) => {
-  let searchStr = args.join('%20');
-  const api = 'https://en.wikipedia.org/w/api.php';
+const buildSearchString = async (searchStr) => {
   // first search, get the right capitalization, then query
   const apiSearch =
     api +
@@ -32,33 +31,50 @@ const callWiki = async (msg, args) => {
   search = await search.json();
 
   searchStr = search[1][0] ? search[1][0].replace(' ', '%20') : '';
+  return searchStr;
+};
 
-  const query = api + `?format=json&action=query&titles=${searchStr}`;
-  const url = query + '&prop=extracts&exintro&explaintext&redirects=1';
-  const imageURL = query + '&prop=pageimages&pithumbsize=100';
+const joinWithApiStrDelimiter = (arr) => {
+  return arr.join('%20');
+};
 
-  const maxLength = 880;
-
-  // get excerpt
+const fetchWikiPage = async (url) => {
   let result = await fetch(url);
   result = await result.json();
   if (!result.query) throw new Error(`Nopes! Couldn't find that in da wiki 😿`);
   result = Object.values(result.query.pages)[0];
+  return result;
+};
 
-  const pageID = result.pageid;
+const fetchWikiImage = async (url, pageID) => {
+  let img = await fetch(url);
+  img = await img.json();
+
+  return img.query.pages[pageID] && img.query.pages[pageID].thumbnail ?
+      img.query.pages[pageID].thumbnail.source :
+      null;
+};
+
+const callWiki = async (msg, args) => {
+  const searchStr = await buildSearchString(joinWithApiStrDelimiter(args));
+
+  const query = `${api}?format=json&action=query&titles=${searchStr}`;
+  const wikiUrl = `${query}&prop=extracts&exintro&explaintext&redirects=1`;
+
+  const wikiPage = await fetchWikiPage(wikiUrl);
+
+  const pageID = wikiPage.pageid;
 
   if (!pageID) throw new Error(`Nopes! Couldn't find that in da wiki 😿`);
 
   // get image
-  let img = await fetch(imageURL);
-  img = await img.json();
-  img =
-    img.query.pages[pageID] && img.query.pages[pageID].thumbnail ?
-      img.query.pages[pageID].thumbnail.source :
-      null;
+  const imageURL = `${query}&prop=pageimages&pithumbsize=100`;
 
-  const title = result.title;
-  const extract = result.extract;
+  const img = await fetchWikiImage(imageURL, pageID);
+
+  const title = wikiPage.title;
+  const extract = wikiPage.extract;
+  const maxLength = 880;
 
   const excerpt =
     extract.length >= maxLength ?
