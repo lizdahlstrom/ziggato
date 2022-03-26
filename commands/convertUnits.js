42dde4dd7f3a5a61c0bf8bb2893a6723e3cb15ed
Liz Dahlström <liz.dahlstrom@gmail.com>
2022-03-26 19:25:00 +0100

    Refactor conversion command

--- commands/convertUnits.js
+++ commands/convertUnits.js
@@ -1,65 +1,63 @@
 const convert = require('convert-units');
-const Discord = require('discord.js');
-const {palette} = require('../config.json');
+const embedBuilder = require('./helpers/embedBuilder.js');
 
-const buildEmbed = (output, userName) => {
-  const embed = new Discord.MessageEmbed()
-      .setColor(palette.mid1)
-      .setAuthor('Unit converter 🐈')
-      .setFooter(`Requested by ${userName}`)
-      .setTimestamp(new Date());
+const _listUnits = (unit) => {
+  if (!unit) return `${convert().possibilities().join(', ')}`;
 
-  if (output.length >= 250) {
-    embed.setTitle('Possible conversions');
-    embed.setDescription(output);
-  } else {
-    embed.setTitle(output);
+  try {
+    return `<${unit}> can be converted to: ${convert()
+        .from(unit)
+        .possibilities()
+        .join(', ')}`;
+  } catch (err) {
+    throw new Error('Invalid conversion');
   }
+};
 
-  return embed;
+const _convert = (original, target, amount) => {
+  const originalDescr = convert().describe(original);
+  const targetDescr = convert().describe(target);
+  const result = convert(amount).from(original).to(target);
+
+  return `${amount} ${
+      amount > 1 ? originalDescr.plural : originalDescr.singular
+  } =  ${result.toFixed(2)} ${
+      result > 1 ? targetDescr.plural : targetDescr.singular
+  }`;
+};
+
+const _buildMessage = (author, conversion) => {
+  const isMaxLength = conversion.length >= 250;
+
+  return embedBuilder.buildEmbed(
+      'Unit converter 🐈',
+      isMaxLength ? 'Possible conversions': conversion,
+      author,
+      isMaxLength? conversion : '');
 };
 
 module.exports = {
   name: 'units',
   description: 'Units converter',
   async execute(msg, args) {
-    let output = '';
-
-    if (args[0] === 'list') {
-      if (args[1]) {
-        try {
-          output = `<${args[1]}> can be converted to: ${convert()
-              .from(args[1])
-              .possibilities()
-              .join(', ')}`;
-        } catch (err) {
-          throw new Error('Invalid conversion');
-        }
-      } else {
-        output = `${convert().possibilities().join(', ')}`;
-      }
-    } else if (args.length === 3) {
-      const original = args[0];
-      const target = args[1];
-      const amount = args[2];
-
-      try {
-        const originalDescr = convert().describe(original);
-        const targetDescr = convert().describe(target);
-        const result = convert(amount).from(original).to(target);
-
-        output = `${amount} ${
-          amount > 1 ? originalDescr.plural : originalDescr.singular
-        } =  ${result.toFixed(2)} ${
-          result > 1 ? targetDescr.plural : targetDescr.singular
-        }`;
-      } catch (err) {
-        throw new Error('Invalid output 😿');
-      }
-    } else {
-      throw new Error('Missing arguments');
+    if (args.length > 3 && args[0] !== 'list') {
+      throw new Error('Invalid arguments');
     }
 
-    msg.channel.send(buildEmbed(output, msg.author.username));
+    let output = '';
+
+    try {
+      if (args[0] === 'list') {
+        output = _listUnits(args[1]);
+      } else if (args.length === 3) {
+        const [original, target, amount] = args;
+
+        output = _convert(original, target, amount);
+      }
+    } catch (err) {
+      output = err.message;
+    }
+
+    msg.channel.send(_buildMessage(msg.author.username, output));
   },
 };
