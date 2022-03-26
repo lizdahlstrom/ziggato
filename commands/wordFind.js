2e04c1d3ab19f8d9af21fda22f5f81b9bcd50493
Liz Dahlström <liz.dahlstrom@gmail.com>
2022-03-26 17:42:37 +0100

    Refactor wordfind

--- commands/wordFind.js
+++ commands/wordFind.js
@@ -1,45 +1,51 @@
 const fetch = require('node-fetch');
 const embedBuilder = require('./helpers/embedBuilder.js');
 const api = 'https://api.datamuse.com/words';
+const EMBED_MAX_LENGTH = 110;
 
-const getSynonyms = async (queryType, word) => {
+const _fetchRes = async (queryType, word) => {
   const url = `${api}?rel_${queryType}=${word}`;
-
   let res = await fetch(url);
   res = await res.json();
 
   return res;
 };
 
+const _limitStrLength = (text, max) => {
+  return text.substring(0, (text + '\n').lastIndexOf('\n', max));
+};
+
+
+const _formatWordArr =(arr) => {
+  arr = arr.map((word) => `• ${word.word}`).join('\n');
+
+  if (arr.length >= EMBED_MAX_LENGTH) {
+    arr = _limitStrLength(arr, EMBED_MAX_LENGTH);
+  }
+
+  return arr;
+};
+
+const _buildMessage = (result, author, word) => {
+  return embedBuilder.buildEmbed('Word-find 🐈', _formatWordArr(result),
+      author, `From *"${word}"*`);
+};
+
 module.exports = {
   name: 'wordfind',
   description: 'Word-find',
   async execute(msg, args) {
-    if (args.length < 1) throw new Error('Missing arguments');
+    if (args.length < 1) return;
 
     const query = args.length === 2 ? args[0] : 'syn';
+    if (query !== 'syn' && query !== 'rhy') return;
+
     const word = args.length === 2 ? args[1] : args[0];
-    let result = '';
-    const maxLength = 110;
 
-    if (query === 'syn' || query === 'rhy') {
-      const res = await getSynonyms(query, word);
-      result = res.map((word) => `• ${word.word}`);
-      result = result.join('\n');
-    } else {
-      throw new Error('Invalid query, must be "syn" or "rhy"');
-    }
+    const res = await _fetchRes(query, word);
 
-    result =
-      result.length >= maxLength ?
-        result.substring(0, (result + '\n').lastIndexOf('\n', maxLength)) :
-        result;
-
-    if (result) {
-      msg.channel.send(
-          embedBuilder.buildEmbed('Word-find 🐈', result,
-              msg.author.username, `From *"${word}"*`),
-      );
-    }
+    msg.channel.send(
+        _buildMessage(res, msg.author.username, word),
+    );
   },
 };
