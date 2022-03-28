e40d9e9991ad3f9b7e12873f34622f0634191b1c
Liz Dahlström <liz.dahlstrom@gmail.com>
2022-03-28 20:13:34 +0200

    Refactor watch2gether code

--- commands/watch2gether.js
+++ commands/watch2gether.js
@@ -2,16 +2,12 @@ const fetch = require('node-fetch');
 const embedBuilder = require('./helpers/embedBuilder.js');
 
 const createRoom = async (url) => {
-  const KEY = process.env.WATCH2GETHER_KEY;
-
-  const body = {
-    'w2g_api_key': KEY,
-    'share': url,
-  };
-
   const res = await fetch('https://w2g.tv/rooms/create.json', {
     method: 'post',
-    body: JSON.stringify(body),
+    body: JSON.stringify({
+      'w2g_api_key': process.env.WATCH2GETHER_KEY,
+      'share': url,
+    }),
     headers: {'Content-Type': 'application/json'},
   });
 
@@ -24,11 +20,7 @@ module.exports = {
   name: 'watch',
   description: 'Watch videos together',
   async execute(msg, args) {
-    const isValidURL = true;
-
-    if (!isValidURL) throw new Error('not valid url');
-
-    const url = args[0];
+    const [url] = args;
     const roomUrl = await createRoom(url);
     const embed = embedBuilder.buildEmbed('Watch2Gether 🐈', roomUrl,
         msg.author.username);
